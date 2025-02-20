61fb1a5d17c7f73352c7a30a7dc763c9ba54069d
bvikash259 <59353671+bvikash259@users.noreply.github.com>
2025-02-20 19:17:23 +0530

    Update backend.py

--- backend.py
+++ backend.py
@@ -9,7 +9,7 @@ from langchain_community.llms import OpenAI
 from langchain_community.vectorstores import Pinecone
 from langchain_ollama import ChatOllama
 from langchain_core.output_parsers import StrOutputParser
-from pinecone import  ServerlessSpec    
+#from pinecone import  ServerlessSpec    
 from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain_community.document_loaders import PyPDFLoader
 from langchain.chains import create_retrieval_chain
